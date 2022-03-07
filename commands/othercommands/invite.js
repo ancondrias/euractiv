4951042d0df78fa7bb40a098f1c5fae2366e4bfb
ancondrias <80352981+ancondrias@users.noreply.github.com>
2022-03-07 11:45:16 +0100

    Update invite.js

--- commands/othercommands/invite.js
+++ commands/othercommands/invite.js
@@ -11,12 +11,12 @@ module.exports = {
     const embedInvite = new Discord.MessageEmbed();
 
     embedInvite.setColor('#FF6600')
-    .setAuthor('VUB Digital Campus', 'https://i.imgur.com/hRIWwz0.png', 'https://vub.be/')
+    .setAuthor('Euractiv', 'https://i.imgur.com/hRIWwz0.png', 'https://vub.be/')
     .setThumbnail('https://i.imgur.com/hRIWwz0.png')
-    .setTitle('Click here to invite Foxxy Bot to your server')
-    .setURL('https://discord.com/api/oauth2/authorize?client_id=950329706933284904&permissions=3072&scope=bot')
+    .setTitle('Click here to invite the Euractiv Bot to your server')
+    .setURL('https://discord.com/api/oauth2/authorize?client_id=950329706933284904&permissions=19456&scope=bot')
     .setDescription('**For any questions get in touch with:** Høpe#1030');
-    //.setImage('https://i.imgur.com/hRIWwz0.png', 'https://discord.com/api/oauth2/authorize?client_id=950329706933284904&permissions=3072&scope=bot')
+    //.setImage('https://i.imgur.com/hRIWwz0.png', 'https://discord.com/api/oauth2/authorize?client_id=950329706933284904&permissions=19456&scope=bot')
 
 
     // Functions
